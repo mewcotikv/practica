ab6dd78709ec7752c567e93c7a4e572d0165d6cd
mashina mewcotik <lixandrusendrica@gmail.com>
2026-05-21 14:30:51 +0300

    Delete REDConstruct directory

--- REDConstruct/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows;
-
-namespace REDConstruct
-{
-    public partial class App : Application
-    {
-    }
-}
--- REDConstruct/Models/Obiectiv.cs
+++
@@ -1,9 +0,0 @@
-namespace REDConstruct.Models
-{
-    public class Obiectiv
-    {
-        public int Id { get; set; }
-        public string Nume { get; set; }
-        public string Descriere { get; set; }
-    }
-}
